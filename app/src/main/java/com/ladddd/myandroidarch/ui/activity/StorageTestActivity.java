2003e2fb30b94cbc253a6426bda59275b3fd6a98
陈伟达 <chenweida2317@tops001.com>
2017-07-19 18:37:59 +0800

    storage test db

--- app/src/main/java/com/ladddd/myandroidarch/ui/activity/StorageTestActivity.java
+++ app/src/main/java/com/ladddd/myandroidarch/ui/activity/StorageTestActivity.java
@@ -5,7 +5,10 @@ import android.content.Intent;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import com.ladddd.myandroidarch.MyApplication;
 import com.ladddd.myandroidarch.R;
+import com.ladddd.myandroidarch.model.dao.TestDao;
+import com.ladddd.myandroidarch.model.dao.TestDao_;
 import com.ladddd.mylib.BaseActivity;
 import com.ladddd.mylib.utils.CacheUtils;
 import com.ladddd.mylib.utils.SPUtils;
@@ -13,12 +16,17 @@ import com.ladddd.mylib.utils.SPUtils;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
+import io.objectbox.Box;
+import io.objectbox.query.Query;
+import io.objectbox.query.QueryBuilder;
 
 /**
  * Created by 陈伟达 on 2017/7/11.
  */
 
 public class StorageTestActivity extends BaseActivity {
+
+    private Box<TestDao> testBox;
 
     @BindView(R.id.et_sp) EditText et_sp;
     @BindView(R.id.et_cache) EditText et_cache;
@@ -34,7 +42,9 @@ public class StorageTestActivity extends BaseActivity {
         CacheUtils.getInstance("StorageTest").put("test", newValue);
     }
     @OnClick(R.id.btn_save_db) void saveDb() {
-
+        String newValue = et_db.getText().toString();
+        TestDao testDao = new TestDao("test", newValue);
+        testBox.put(testDao);
     }
     @OnClick(R.id.btn_load_sp) void loadSp() {
         String value = SPUtils.getInstance().getString("test");
@@ -45,7 +55,13 @@ public class StorageTestActivity extends BaseActivity {
         tv_value.setText(value);
     }
     @OnClick(R.id.btn_load_db) void loadDb() {
-
+        QueryBuilder<TestDao> builder = testBox.query().equal(TestDao_.key, "test");
+        Query<TestDao> query =  builder.build();
+        TestDao testDao = query.findFirst();
+        if (testDao != null) {
+            String value = testDao.getValue();
+            tv_value.setText(value);
+        }
     }
 
     public static void launch(Context context) {
@@ -61,6 +77,6 @@ public class StorageTestActivity extends BaseActivity {
 
     @Override
     protected void initData() {
-
+        testBox = ((MyApplication) getApplication()).getBoxStore().boxFor(TestDao.class);
     }
 }
