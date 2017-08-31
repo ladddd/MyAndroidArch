f24236d0363e4b73fccfa1f6a39f4dc7908a1cf1
陈伟达 <chenweida2317@tops001.com>
2017-08-31 13:47:31 +0800

    7.1 app shortcut demo

--- app/src/main/java/com/ladddd/myandroidarch/MyApplication.java
+++ app/src/main/java/com/ladddd/myandroidarch/MyApplication.java
@@ -1,14 +1,25 @@
 package com.ladddd.myandroidarch;
 
+import android.annotation.TargetApi;
 import android.content.Context;
+import android.content.Intent;
+import android.content.pm.ShortcutInfo;
+import android.content.pm.ShortcutManager;
+import android.graphics.drawable.Icon;
+import android.os.Build;
 import android.support.annotation.ColorInt;
 import android.support.annotation.ColorRes;
 
 import com.bilibili.magicasakura.utils.ThemeUtils;
 import com.ladddd.myandroidarch.model.dao.MyObjectBox;
+import com.ladddd.myandroidarch.ui.activity.HorizonAndHeaderPtrActivity;
+import com.ladddd.myandroidarch.ui.activity.MainActivity;
+import com.ladddd.myandroidarch.ui.activity.PtrActivity;
 import com.ladddd.myandroidarch.utils.ThemeHelper;
 import com.ladddd.mylib.MyApp;
 import com.ladddd.mylib.config.AppConfig;
+
+import java.util.Arrays;
 
 import io.objectbox.BoxStore;
 
@@ -24,6 +35,43 @@ public class MyApplication extends MyApp implements ThemeUtils.switchColor {
     public void onCreate() {
         super.onCreate();
         ThemeUtils.setSwitchColor(this);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N_MR1) {
+            initShortcut();
+        }
+    }
+
+    @TargetApi(Build.VERSION_CODES.N_MR1)
+    public void initShortcut() {
+        ShortcutManager sm = getSystemService(ShortcutManager.class);
+        if (sm == null) {
+            return;
+        }
+        ShortcutInfo shortcutInfo = new ShortcutInfo.Builder(this, "ptr")
+                .setRank(1)
+                .setIcon(Icon.createWithResource(this, R.mipmap.ic_test2))
+                .setShortLabel(getString(R.string.ptr_shortcut_short_label))
+                .setLongLabel(getString(R.string.ptr_shortcut_long_label))
+                .setDisabledMessage(getString(R.string.ptr_disabled_message))
+                .setIntents(new Intent[] {
+                        //清空当前任务栈，重新组织
+                        new Intent(this, MainActivity.class).setAction(Intent.ACTION_MAIN).setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK),
+                        new Intent(this, PtrActivity.class).setAction(Intent.ACTION_MAIN)
+                })
+                .build();
+        ShortcutInfo shortcutInfo2 = new ShortcutInfo.Builder(this, "hptr")
+                .setRank(0)
+                .setIcon(Icon.createWithResource(this, R.mipmap.ic_test3))
+                .setShortLabel(getString(R.string.hptr_shortcut_short_label))
+                .setLongLabel(getString(R.string.hptr_shortcut_long_label))
+                .setDisabledMessage(getString(R.string.hptr_disabled_message))
+                .setIntents(new Intent[] {
+                        //清空当前任务栈，重新组织
+                        new Intent(this, MainActivity.class).setAction(Intent.ACTION_MAIN).setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK),
+                        new Intent(this, HorizonAndHeaderPtrActivity.class).setAction(Intent.ACTION_MAIN)
+                })
+                .build();
+        sm.addDynamicShortcuts(Arrays.asList(shortcutInfo, shortcutInfo2));
     }
 
     public BoxStore getBoxStore()
